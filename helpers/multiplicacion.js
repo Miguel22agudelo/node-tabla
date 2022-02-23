5452f5cd5740c422b2ae5799e2169405eec11803
Miguel Ángel Agudelo Lopera <32880967+Miguel22agudelo@users.noreply.github.com>
2022-02-23 10:20:14 -0500

    Se agregan nuevas características de Node

--- helpers/multiplicacion.js
+++ helpers/multiplicacion.js
@@ -1,28 +1,30 @@
 const fs = require("fs");
-const colors = require('colors');
-
+const colors = require("colors");
 
 const crearArchivo = async (base, listar, hasta) => {
-  
-  let salida = "";
-  let nombreArchivo = `tabla-${base}.txt`;
-  
-  for (let index = 0; index <= hasta; index++) {
-    salida += `${base} x ${index} = ${index * base}\n`;
-  }
-  
-  if (listar) {
-    console.log(colors.rainbow("==============="));
-    console.log(" Tabla del ", base);
-    console.log(colors.rainbow("==============="));
-    console.log(salida.rainbow);
-  }
+  try {
+    let salida = "";
+    let nombreArchivo = `tabla-${base}.txt`;
 
-  fs.writeFile(`./salida/tabla-${base}.txt`, salida, (err) => {
-    if (err) throw err;
-  });
+    for (let index = 0; index <= hasta; index++) {
+      salida += `${base} x ${index} = ${index * base}\n`;
+    }
 
-  return nombreArchivo;
+    if (listar) {
+      console.log(colors.rainbow("==============="));
+      console.log(" Tabla del ", base);
+      console.log(colors.rainbow("==============="));
+      console.log(salida.rainbow);
+    }
+
+    fs.writeFile(`./salida/tabla-${base}.txt`, salida, (err) => {
+      if (err) throw err;
+    });
+
+    return nombreArchivo;
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
